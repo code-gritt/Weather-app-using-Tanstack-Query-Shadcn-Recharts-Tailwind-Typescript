35fbb974aeb161e33ee2ab8f8afb512373fe5c96
gokul <gokulchandan24@gmail.com>
2024-11-16 19:09:27 +0530

    app done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import { Button } from "./components/ui/button";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "./components/ui/sonner";
+import { WeatherDashboard } from "./pages/weather-dashboard";
+import { Layout } from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CityPage } from "./pages/city-page";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+          <Toaster richColors />
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/api/config.ts
@@ -0,0 +1,9 @@
+export const API_CONFIG = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5",
+  GEO: "https://api.openweathermap.org/geo/1.0",
+  API_KEY: "f9ca1cb9a68ea6fcca335051c754cd0c",
+  DEFAULT_PARAMS: {
+    units: "metric",
+    appid: "f9ca1cb9a68ea6fcca335051c754cd0c",
+  },
+};
